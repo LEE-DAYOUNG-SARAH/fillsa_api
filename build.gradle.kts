b036afa409462545d6928ecdfc80527986cf7c2a
LEE-DAYOUNG-SARAH <ldy8536@gmail.com>
2025-04-08 22:09:26 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("plugin.jpa") version "1.9.25"
+}
+
+group = "com.fillsa"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+configurations {
+    configureEach {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+    }
+}
+
+val springBootVersion = "3.0.7"
+val mysqlConnectorVersion = "8.0.33"
+val kotlinVersion = "1.8.22"
+val jacksonVersion = "2.14.2"
+val springDocVersion = "2.0.4"
+val kotlinLoggingVersion = "3.0.5"
+
+dependencies {
+    // spring
+    implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-security:$springBootVersion")
+
+    // database
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:$springBootVersion")
+    implementation("com.mysql:mysql-connector-j:$mysqlConnectorVersion")
+    runtimeOnly("com.h2database:h2")
+
+    // kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+
+    // logging
+    implementation("io.github.microutils:kotlin-logging-jvm:$kotlinLoggingVersion")
+    implementation("org.springframework.boot:spring-boot-starter-log4j2:$springBootVersion")
+
+    // swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocVersion")
+
+    // test
+    testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.security:spring-security-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "fillsa_api"
