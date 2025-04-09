b9f29a31abcf72c901a4480a352f82e72415615b
LEE-DAYOUNG-SARAH <ldy8536@gmail.com>
2025-04-09 11:17:43 +0900

    feat: Quote, DailyQuote 엔티티 추가

---
+++ src/main/kotlin/com/fillsa/fillsa_api/common/converter/QuoteCategoryConverter.kt
@@ -0,0 +1,16 @@
+package com.fillsa.fillsa_api.common.converter
+
+import com.fillsa.fillsa_api.domain.quote.entity.Quote
+import jakarta.persistence.AttributeConverter
+import jakarta.persistence.Converter
+
+@Converter(autoApply = true)
+class QuoteCategoryConverter : AttributeConverter<Quote.QuoteCategory, String> {
+    override fun convertToDatabaseColumn(attribute: Quote.QuoteCategory?): String? {
+        return attribute?.value
+    }
+
+    override fun convertToEntityAttribute(value: String?): Quote.QuoteCategory? {
+        return Quote.QuoteCategory.fromValue(value)
+    }
+}
