d86987a4566f88a3ebf19ce97dc2bff31435d8d0
LEE-DAYOUNG-SARAH <ldy8536@gmail.com>
2025-04-10 11:10:09 +0900

    feat: Notice entity 추가

---
+++ src/main/kotlin/com/fillsa/fillsa_api/domain/notice/entity/Notice.kt
@@ -0,0 +1,18 @@
+package com.fillsa.fillsa_api.domain.notice.entity
+
+import com.fillsa.fillsa_api.common.entity.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "notices")
+class Notice(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val notice_seq: Long = 0L,
+
+    @Column(nullable = false)
+    var title: String,
+
+    @Column(nullable = false)
+    var content: String
+): BaseEntity()
