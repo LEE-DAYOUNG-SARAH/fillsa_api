74c56b600a7c092b4ea986283a0ae8c35dc45e4e
LEE-DAYOUNG-SARAH <ldy8536@gmail.com>
2025-09-05 13:19:24 +0900

    fi: BaseEntity createAt var 변경

--- src/main/kotlin/store/fillsa/fillsa_api/common/entity/BaseEntity.kt
+++ src/main/kotlin/store/fillsa/fillsa_api/common/entity/BaseEntity.kt
@@ -13,7 +13,7 @@ import java.time.LocalDateTime
 abstract class BaseEntity(
     @CreatedDate
     @Column(nullable = false, updatable = false)
-    val createdAt: LocalDateTime = LocalDateTime.now(),
+    var createdAt: LocalDateTime = LocalDateTime.now(),
 
     @LastModifiedDate
     @Column(nullable = false)
